a8e9e8bb23b47f076c04328d70d638c0ffa540f4
ArielMonroy <ariel.monroy@ucb.edu.bo>
2023-04-08 19:21:43 -0400

    adding navabar and background to InternshipForm

--- src/app/services/internship.service.ts
+++ src/app/services/internship.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InternshipService {
-  URL: string = 'http://192.168.194.48:8080/internship';
   constructor(private http: HttpClient) {}
 
   saveInternship(internship: any) {
-    return this.http.post(this.URL, internship);
+    return this.http.post(`${environment.API_URL}/internship`, internship);
   }
 }
